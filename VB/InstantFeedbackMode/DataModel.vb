ba6ffe90ebeb452f1fd061aacd6f211d6d74b595
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/InstantFeedbackMode/DataModel.vb
@@ -0,0 +1,47 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports DevExpress.Xpo
+
+Namespace InstantFeedbackMode
+	Public Class TestObject
+		Inherits XPObject
+		Private _subject As String
+		Public Property Subject() As String
+			Get
+				Return _subject
+			End Get
+			Set(ByVal value As String)
+				SetPropertyValue(Of String)("Subject", _subject, value)
+			End Set
+		End Property
+		Private _sent As DateTime
+		Public Property Sent() As DateTime
+			Get
+				Return _sent
+			End Get
+			Set(ByVal value As DateTime)
+				SetPropertyValue(Of DateTime)("Sent", _sent, value)
+			End Set
+		End Property
+
+		Private _hasAttachment As Boolean
+		Public Property HasAttachment() As Boolean
+			Get
+				Return _hasAttachment
+			End Get
+			Set(ByVal value As Boolean)
+				SetPropertyValue(Of Boolean)("HasAttachment", _hasAttachment, value)
+			End Set
+		End Property
+		Public Sub New(ByVal session As Session)
+			MyBase.New(session)
+		End Sub
+		Public Sub New()
+			MyBase.New(Session.DefaultSession)
+		End Sub
+		Public Overrides Sub AfterConstruction()
+			MyBase.AfterConstruction()
+		End Sub
+	End Class
+End Namespace
+
